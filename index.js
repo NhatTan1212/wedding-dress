d4f496895172272ad638e167499260757146b755
NhatTan1212 <honhattan121@gmail.com>
2024-03-25 15:51:14 +0700

    fix deploy vercel

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const userRouter = require('./routes/userRoute');
 const bodyParser = require('body-parser');
 const authRoute = require('./routes/authRoute')
 const productRoute = require('./routes/productRoute');
+const { database } = require('./config');
 
 require('dotenv').config();
 
@@ -18,6 +19,17 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Wellcome to NT Web')
+})
+app.get('/user2', async (req, res) => {
+    try {
+        const USERS = database.collection('USERS');
+        const users = await USERS.find().toArray();
+        console.log('List of users:', users);
+        res.send(users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).send('Internal Server Error');
+    }
 })
 app.use('/auth', authRoute);
 app.use('/user', userRouter);
